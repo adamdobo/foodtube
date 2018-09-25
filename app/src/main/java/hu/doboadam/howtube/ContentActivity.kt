ebabf3a75c61489a4f80851f0f5f0c7a505af0fe
Dobo Adam <dobo.adam@autsoft.hu>
2018-09-25 18:43:52 +0200

    Implement google and facebook login.

---
+++ app/src/main/java/hu/doboadam/howtube/ContentActivity.kt
@@ -0,0 +1,13 @@
+package hu.doboadam.howtube
+
+import android.os.Bundle
+import android.app.Activity
+
+class ContentActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_content)
+    }
+
+}
--- app/src/main/java/hu/doboadam/howtube/LoginActivity.kt
+++ app/src/main/java/hu/doboadam/howtube/LoginActivity.kt
@@ -1,25 +1,38 @@
 package hu.doboadam.howtube
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class LoginActivity : AppCompatActivity() {
 
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
     private val RC_SIGN_IN: Int = 100
+    private lateinit var callbackManager: CallbackManager
+
+    companion object {
+        private const val EMAIL = "email"
+        private const val PUBLIC_PROFILE = "public_profile"
+        private const val USER_FRIENDS = "user_friends"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,9 +42,39 @@ class MainActivity : AppCompatActivity() {
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build()
+        callbackManager = CallbackManager.Factory.create()
+        facebookLoginButton.setReadPermissions(listOf(EMAIL, PUBLIC_PROFILE))
+        facebookLoginButton.registerCallback(callbackManager, object: FacebookCallback<LoginResult> {
+            override fun onCancel() {
+                //Do nothing
+            }
+
+            override fun onError(error: FacebookException?) {
+                Snackbar.make(mainLayout, "Login failed :(", Snackbar.LENGTH_LONG).show()
+            }
+
+            override fun onSuccess(result: LoginResult) {
+                handleFacebookAccessToken(result.accessToken)
+            }
+
+        })
         googleSignInClient = GoogleSignIn.getClient(this, googleSingInOptions)
         googleSignInButton.setOnClickListener {
-            signIn()
+            signInWithGoogle()
+        }
+        facebookSignInButton.setOnClickListener {
+            facebookLoginButton.performClick()
+        }
+    }
+
+    private fun handleFacebookAccessToken(accessToken: AccessToken) {
+        val credential = FacebookAuthProvider.getCredential(accessToken.token)
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {
+            if(it.isSuccessful){
+                advance()
+            } else {
+                Snackbar.make(mainLayout, "Login failed :(", Snackbar.LENGTH_LONG).show()
+            }
         }
     }
 
@@ -43,7 +86,7 @@ class MainActivity : AppCompatActivity() {
         //TODO("if user is logged in do something")
     }
 
-    private fun signIn(){
+    private fun signInWithGoogle(){
         startActivityForResult(googleSignInClient.signInIntent, RC_SIGN_IN)
     }
 
@@ -57,9 +100,12 @@ class MainActivity : AppCompatActivity() {
                 val account = task.result
                 firebaseAuthWithGoogle(account)
             } catch (e: ApiException) {
-                Log.e(this@MainActivity.localClassName, "Google Sign-in failed", e)
+                Log.e(this@LoginActivity.localClassName, "Google Sign-in failed", e)
             }
+        } else {
+            callbackManager.onActivityResult(requestCode, resultCode, data)
         }
+
     }
 
     private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
@@ -67,11 +113,14 @@ class MainActivity : AppCompatActivity() {
         firebaseAuth.signInWithCredential(credential)
                 .addOnCompleteListener {
                     if(it.isSuccessful){
-                        Snackbar.make(mainLayout, "Wow it worked! ${firebaseAuth.currentUser?.uid}", Snackbar.LENGTH_LONG).show()
-                        //TODO("user logged in")
+                        advance()
                     } else {
-
+                        Snackbar.make(mainLayout, "Login failed :(", Snackbar.LENGTH_LONG).show()
                     }
                 }
+    }
+
+    private fun advance() {
+        startActivity(Intent(this, ContentActivity::class.java))
     }
 }
